4d0a51d908d040c229709ff4c9db2bf7478e68c1
nadun-k-s <nadunkaushalya2015@gmail.com>
2025-05-04 18:11:59 +0530

    dorp down correction

--- app/views/home/section1.php
+++ app/views/home/section1.php
@@ -4,7 +4,7 @@
             <a class="navbar-brand" href="https://cogentsolutions.ae/" style="margin-left: 15px;">
                 <img src="https://cogentsolutions.ae/views/img/logos/cogent-logo-01.png" alt="Logo" style="height: 60px; margin-top: 10px;">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="margin-right: 15px;background-color: white;">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="margin-right: 15px;background-color: white;">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
